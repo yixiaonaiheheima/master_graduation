db3adc95aab94ee1d82f66cf4ce9e22efaa564ac
yixiaonaiheheima <23020171153067@stu.xmu.edu.cn>
2019-12-27 16:32:29 +0800

    remove reduntant files

--- models/pointnet.py
+++ models/pointnet.py
@@ -39,7 +39,7 @@ class STN3d(nn.Module):
 
         iden = Variable(torch.from_numpy(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32))).view(1, 9).repeat(batchsize, 1)
         if x.is_cuda:
-            iden = iden.cuda()
+            iden = iden.to(x.device)
         x = x + iden
         x = x.view(-1, 3, 3)  # (b, 3, 3)
         return x
@@ -85,9 +85,11 @@ class STNkd(nn.Module):
 
 
 class PointNetfeat(nn.Module):
-    def __init__(self, global_feat=True, feature_transform=False):
+    def __init__(self, global_feat=True, feature_transform=False, input_transform=True):
         super(PointNetfeat, self).__init__()
-        self.stn = STN3d()
+        self.input_transform = input_transform
+        if input_transform:
+            self.stn = STN3d()
         self.conv1 = torch.nn.Conv1d(3, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 1024, 1)
@@ -109,10 +111,11 @@ class PointNetfeat(nn.Module):
         trans_feat: tensor(b, 64, 64)
         """
         n_pts = x.size()[2]
-        trans = self.stn(x)  # (b, 3, 3)
-        x = x.transpose(2, 1)  # (b, n, 3)
-        x = torch.bmm(x, trans)  # (b, n, 3)
-        x = x.transpose(2, 1)  # (b, 3, n)
+        if self.input_transform:
+            trans = self.stn(x)  # (b, 3, 3)
+            x = x.transpose(2, 1)  # (b, n, 3)
+            x = torch.bmm(x, trans)  # (b, n, 3)
+            x = x.transpose(2, 1)  # (b, 3, n)
         x = F.relu(self.bn1(self.conv1(x)))  # (b, 64, n)
 
         if self.feature_transform:
@@ -120,26 +123,23 @@ class PointNetfeat(nn.Module):
             x = x.transpose(2, 1)  # (b, n, 64)
             x = torch.bmm(x, trans_feat)  # (b, n, 64)
             x = x.transpose(2,1)  # (b, 64, n)
-        else:
-            trans_feat = None
-
         pointfeat = x  # (b, 64, n)
         x = F.relu(self.bn2(self.conv2(x)))  # (b, 128, n)
         x = self.bn3(self.conv3(x))  # (b, 1024, n)
         x = torch.max(x, 2, keepdim=True)[0]  # (b, 1024, 1)
         x = x.view(-1, 1024)  # (b, 1024)
         if self.global_feat:
-            return x, trans, trans_feat
+            return x
         else:
             x = x.view(-1, 1024, 1).repeat(1, 1, n_pts)  # (b, 1024, n)
-            return torch.cat([x, pointfeat], 1), trans, trans_feat  # (b, 1088, n), (b, 3, 3), (b, 64, 64)
+            return torch.cat([x, pointfeat], 1)  # (b, 1088, n), (b, 3, 3), (b, 64, 64)
 
 
 class PointNetCls(nn.Module):
     def __init__(self, k=2, feature_transform=False):
         super(PointNetCls, self).__init__()
         self.feature_transform = feature_transform
-        self.feat = PointNetfeat(global_feat=True, feature_transform=feature_transform)
+        self.feat = PointNetfeat(global_feat=True, feature_transform=feature_transform, input_transform=False)
         self.fc1 = nn.Linear(1024, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, k)
@@ -156,9 +156,46 @@ class PointNetCls(nn.Module):
         return F.log_softmax(x, dim=1), trans, trans_feat
 
 
+# class PointNetDenseCls(nn.Module):
+#     def __init__(self, k=2, feature_transform=False):
+#         super(PointNetDenseCls, self).__init__()
+#         self.k = k
+#         self.feature_transform = feature_transform
+#         self.feat = PointNetfeat(global_feat=False, feature_transform=feature_transform)
+#         self.conv1 = torch.nn.Conv1d(1088, 512, 1)
+#         self.conv2 = torch.nn.Conv1d(512, 256, 1)
+#         self.conv3 = torch.nn.Conv1d(256, 128, 1)
+#         self.conv4 = torch.nn.Conv1d(128, self.k, 1)
+#         self.bn1 = nn.BatchNorm1d(512)
+#         self.bn2 = nn.BatchNorm1d(256)
+#         self.bn3 = nn.BatchNorm1d(128)
+#
+#     def forward(self, x):
+#         """
+#
+#         :param x: tensor(b, 3, n)
+#         :return:
+#         x: (b, n, num_classes)
+#         trans: (b, 3, 3)
+#         trans_feat: (b, 64, 64)
+#         """
+#         batchsize = x.size()[0]
+#         n_pts = x.size()[2]
+#         x, trans, trans_feat = self.feat(x)
+#         x = F.relu(self.bn1(self.conv1(x)))  # (b, 512, n)
+#         x = F.relu(self.bn2(self.conv2(x)))  # (b, 256, n)
+#         x = F.relu(self.bn3(self.conv3(x)))  # (b, 128, n)
+#         x = self.conv4(x)  # (b, k, n)
+#         x = x.transpose(2, 1).contiguous()  # (b, n, k)
+#         x = F.log_softmax(x.view(-1, self.k), dim=-1)  # (b*n, k)
+#         x = x.view(batchsize, n_pts, self.k)  # (b, n, k)
+#         return x, trans, trans_feat
+
+
 class PointNetDenseCls(nn.Module):
-    def __init__(self, k=2, feature_transform=False):
+    def __init__(self, k=2, with_rgb=False, feature_transform=False):
         super(PointNetDenseCls, self).__init__()
+        self.with_rgb = with_rgb
         self.k = k
         self.feature_transform = feature_transform
         self.feat = PointNetfeat(global_feat=False, feature_transform=feature_transform)
@@ -181,7 +218,7 @@ class PointNetDenseCls(nn.Module):
         """
         batchsize = x.size()[0]
         n_pts = x.size()[2]
-        x, trans, trans_feat = self.feat(x)
+        x = self.feat(x)
         x = F.relu(self.bn1(self.conv1(x)))  # (b, 512, n)
         x = F.relu(self.bn2(self.conv2(x)))  # (b, 256, n)
         x = F.relu(self.bn3(self.conv3(x)))  # (b, 128, n)
@@ -189,7 +226,8 @@ class PointNetDenseCls(nn.Module):
         x = x.transpose(2, 1).contiguous()  # (b, n, k)
         x = F.log_softmax(x.view(-1, self.k), dim=-1)  # (b*n, k)
         x = x.view(batchsize, n_pts, self.k)  # (b, n, k)
-        return x, trans, trans_feat
+        return x
+
 
 def feature_transform_regularizer(trans):
     d = trans.size()[1]
